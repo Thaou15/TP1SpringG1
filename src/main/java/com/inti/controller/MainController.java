0fa1cfb7e6c9def1f8e4f61ab28fbf48667b0282
InfoJavaF <florian.belin@itech.fr>
2023-03-15 16:28:39 +0100

    Update

--- src/main/java/com/inti/controller/MainController.java
+++ src/main/java/com/inti/controller/MainController.java
@@ -1,9 +1,68 @@
 package com.inti.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.inti.model.Soliste;
+import com.inti.repository.ISolisteRepository;
+
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	ISolisteRepository isr;
+	
+	@GetMapping("formSoliste")
+	public String formSoliste()
+	{
+		return "formSoliste";
+	}
 
-}
+	@PostMapping("saveSoliste")
+	public String saveSoliste(@ModelAttribute("soliste") Soliste S)
+	{
+		isr.save(S);
+		
+		return "redirect:/formSoliste";
+	}
+		
+	@GetMapping("listeSoliste")
+	public String listeSoliste(Model m)
+	{
+		m.addAttribute("listeS", isr.findAll());
+		
+		return "listeSoliste";
+	}
+	
+	@GetMapping("deleteSoliste/{id}")
+	public String deleteSoliste(@PathVariable("id") int id)
+	{
+		isr.deleteById(id);
+		
+		return "redirect:/listeSoliste";
+	}
+	
+	@GetMapping("modifSoliste/{id}")
+	public String modifSoliste(@PathVariable("id") int id, Model m)
+	{
+		m.addAttribute("soliste", isr.getReferenceById(id));
+		
+		return "modifSoliste";
+	}
+	
+	@PostMapping("updateSoliste")
+	public String updateSoliste(@ModelAttribute("vol") Soliste s)
+	{
+		isr.save(s);
+		
+		return "redirect:/listeSoliste";
+	}
+
+	}
+
--- src/main/java/com/inti/model/Oeuvre.java
+++ src/main/java/com/inti/model/Oeuvre.java
@@ -1,20 +1,34 @@
 package com.inti.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Oeuvre {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nom;
 	private int duree;
 
+	@OneToMany(mappedBy = "oeuvre" ,targetEntity = Soliste.class)
+    
+    private List<Soliste> listeSoliste;
+	
+	
 }
