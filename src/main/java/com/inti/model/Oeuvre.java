fe68a615a51c731388af7488ae6dd7a6e6c1f539
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-15 14:05:19 +0100

    TP1 version 1

---
+++ src/main/java/com/inti/model/Oeuvre.java
@@ -0,0 +1,20 @@
+package com.inti.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Oeuvre {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nom;
+	private int duree;
+
+}
